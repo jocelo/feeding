7fb62b88c8d0c63a99415112fb1c240956cc3c0a
Alfredo Alonso <jocelo@Alfredos-MBP.fios-router.home>
2019-03-09 01:14:27 -0500

    Layout

--- App.js
+++ App.js
@@ -1,11 +1,52 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Alert, TouchableHighlight, Button, TouchableOpacity } from 'react-native';
+import {ToastAndroid} from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hits: []
+    }
+    this.addNewDate = this.addNewDate.bind(this);
+  }
+
+  addNewDate() {
+    const hits = this.state.hits;
+    hits.push(new Date());
+    this.setState({hits: hits});
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View
+        style={{
+          flexDirection: 'column',
+          flex: .8,
+          padding: 30
+        }}>
+
+        <View style={{backgroundColor: 'rgba(100, 100, 255, .4)', flex: 1, flexDirection: 'column'}}>
+        <TouchableOpacity style={{ height: '100%', width: '100%', backgroundColor: 'orange'}}>
+            <Text>My button</Text>
+        </TouchableOpacity>
+
+        </View>
+        <View style={{backgroundColor: 'rgba(100, 100, 255, .4)', flex: 1, flexDirection: 'column'}}>
+          <Button title="press me" onPress={this.addNewDate}></Button>
+        </View>
+        <View style={{backgroundColor: 'rgba(100, 100, 255, .4)', flex: 1, flexDirection: 'column'}}>
+          <Button title="press me" style={{width: '100%'}} onPress={this.addNewDate}></Button>
+        </View>
+        <View style={{backgroundColor: 'rgba(100, 100, 255, .4)', flex: 1, flexDirection: 'column'}}>
+          <Button title="press me" style={{width: '100%'}} onPress={this.addNewDate}></Button>
+        </View>
+        <Text>
+          {this.state.hits.map(item=>{
+            return 'Hello ja!!';
+          })}
+        </Text>
+
       </View>
     );
   }
@@ -18,4 +59,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  ounce1: {
+    backgroundColor: 'orangered',
+    alignSelf: 'stretch',
+  },
+  ounce2: {
+    backgroundColor: 'orange',
+    alignSelf: 'stretch'
+  }
 });
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# feeding
+# Feed the Baby!!
+
+App built with react native that will keep track of the feeding habits of your newborn.
